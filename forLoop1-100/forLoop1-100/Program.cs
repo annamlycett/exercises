7096a0a36d3019e40fee5914fd94bff6ea407691
Anna M. Lycett <annamlycett@gmail.com>
2018-10-05 12:24:22 +0100

    Add a solution for printing #1-100 using a for loop

---
+++ forLoop1-100/forLoop1-100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forLoop1_100
+{
+    class Program
+    {
+        // The purpose of this program is to print numbers from 1 to 100 using the 'for' loop.
+
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 101; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
