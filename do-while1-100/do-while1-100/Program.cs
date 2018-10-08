86e92929c38eb21252a44704987138cce13f7dfd
Anna M. Lycett <annamlycett@gmail.com>
2018-10-08 11:18:55 +0100

    Add a solution for printing #1-100 using do/while

---
+++ do-while1-100/do-while1-100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_while1_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 101);
+            Console.ReadLine();
+        }
+    }
+}
